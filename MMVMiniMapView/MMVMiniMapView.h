c8df1d172bf7e442eac394098a9f2ab01e0d2404
Paul Schifferer <paul@schifferers.net>
2013-12-14 14:12:43 -0800

    Initial Commit

---
+++ MMVMiniMapView/MMVMiniMapView.h
@@ -0,0 +1,13 @@
+//
+//  MMVMiniMapView.h
+//  MMVMiniMapView
+//
+//  Created by Paul Schifferer on 12/14/13.
+//  Copyright (c) 2013 Pilgrimage Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MMVMiniMapView : NSObject
+
+@end
---
+++ MMVMiniMapView/MMVMiniMapView.m
@@ -0,0 +1,13 @@
+//
+//  MMVMiniMapView.m
+//  MMVMiniMapView
+//
+//  Created by Paul Schifferer on 12/14/13.
+//  Copyright (c) 2013 Pilgrimage Software. All rights reserved.
+//
+
+#import "MMVMiniMapView.h"
+
+@implementation MMVMiniMapView
+
+@end
